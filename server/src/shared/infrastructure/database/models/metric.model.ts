f082f87af05a1bd191c1de16dbc31d5adb648317
Elias <eliasgarciadev@gmail.com>
2020-03-19 21:58:22 +0100

    Modify the Metric mongoose schema to stop adding _id in subdocs

--- server/src/shared/infrastructure/database/models/metric.model.ts
+++ server/src/shared/infrastructure/database/models/metric.model.ts
@@ -1,33 +1,37 @@
 import { Schema, Model, model } from "mongoose";
 
+import { AutonomousCommunityData } from "../../../domain/autonomous-community-data.interface";
 import { Metric } from "../interfaces/metric.interface";
 import { MongoDoc } from "../interfaces/mongo-doc.type";
 
 export { MetricModel };
+
+const metricDataSchema = new Schema<AutonomousCommunityData>(
+  {
+    autonomousCommunity: {
+      type: Schema.Types.String,
+      required: true
+    },
+    values: {
+      casos: {
+        type: Schema.Types.Number,
+        required: true
+      },
+      fallecidos: {
+        type: Schema.Types.Number,
+        required: true
+      }
+    }
+  },
+  { _id: false }
+);
 
 const metricSchema = new Schema<Metric>({
   timestamp: {
     type: Schema.Types.Date,
     required: true
   },
-  data: [
-    {
-      autonomousCommunity: {
-        type: Schema.Types.String,
-        required: true
-      },
-      values: {
-        casos: {
-          type: Schema.Types.Number,
-          required: true
-        },
-        fallecidos: {
-          type: Schema.Types.Number,
-          required: true
-        }
-      }
-    }
-  ]
+  data: [metricDataSchema]
 });
 
 const MetricModel: Model<MongoDoc<Metric>> = model<MongoDoc<Metric>>(
