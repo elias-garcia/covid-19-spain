0fc799d2b2f418e8345b98e5561ce1a966439963
Elias <eliasgarciadev@gmail.com>
2020-03-19 21:59:06 +0100

    Fix config validation method

--- server/src/shared/infrastructure/config/config.ts
+++ server/src/shared/infrastructure/config/config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./config.interface";
-import { validate } from "../validation";
+import { validateOne } from "../validation";
 import { configValidationSchema } from "../validation/schemas/config.validation-schema";
 
 export { config };
 
-const config: Config = validate(
+const config: Config = validateOne(
   {
     NODE_ENV: process.env.NODE_ENV || "development",
     API_PORT: process.env.API_PORT || 3000,
