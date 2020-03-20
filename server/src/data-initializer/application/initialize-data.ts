e4ec90a7ed2eff78c02c0dd01f62b00558744319
Elias <eliasgarciadev@gmail.com>
2020-03-20 02:37:10 +0100

    Remove logs in the initialize-data use case

--- server/src/data-initializer/application/initialize-data.ts
+++ server/src/data-initializer/application/initialize-data.ts
@@ -5,7 +5,6 @@ import {
   validateOne,
   validateMany
 } from "../../shared/infrastructure/validation";
-import { logger } from "../../shared/infrastructure/logging";
 import { metricValidationSchema } from "../../shared/infrastructure/validation/schemas/metric.validation-schema";
 import { Metric } from "../../shared/infrastructure/database/interfaces/metric.interface";
 import { MetricModel } from "../../shared/infrastructure/database/models/metric.model";
@@ -15,23 +14,19 @@ import { ScraperConfigModel } from "../../shared/infrastructure/database/models/
 export { initializeData };
 
 async function initializeMetrics(): Promise<void> {
-  logger.info("Initializing Metrics data...");
   const validatedMetricsData: Metric[] = validateMany(
     metricsData,
     metricValidationSchema
   );
   await MetricModel.create(validatedMetricsData);
-  logger.info("Metrics data initialized...");
 }
 
 async function initializeScraperConfig(): Promise<void> {
-  logger.info("Initializing Scraper Config data...");
   const validatedScraperConfigData: ScraperConfig = validateOne(
     scraperConfigData,
     scraperConfigValidationSchema
   );
   await ScraperConfigModel.create(validatedScraperConfigData);
-  logger.info("Scraper Config data initialized...");
 }
 
 async function initializeData(): Promise<void> {
