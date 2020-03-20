60c23256aa78661c19fe4e3dd06a75cd17f5d79c
Elias <eliasgarciadev@gmail.com>
2020-03-20 18:02:35 +0100

    Install node-cron and schedule the scraper to run every hour

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,17 @@
+import * as cron from "node-cron";
+
 import * as database from "./shared/infrastructure/database";
 import * as dataInitializer from "./apps/data-initializer";
 import * as scraper from "./apps/scraper";
 import { logger } from "./shared/infrastructure/logging";
 
+const SCRAPER_CRON_EXPRESSION = "0 * * * *";
+
 async function main(): Promise<void> {
   try {
     await database.connect();
     await dataInitializer.run();
-    await scraper.run();
+    cron.schedule(SCRAPER_CRON_EXPRESSION, scraper.run);
   } catch (error) {
     logger.error(error);
   }
