897cbf4447295197068bed4d7e3b82d195954fc9
Elias <eliasgarciadev@gmail.com>
2020-03-20 02:36:23 +0100

    Improve the data-initializer logs

--- server/src/data-initializer/index.ts
+++ server/src/data-initializer/index.ts
@@ -6,18 +6,24 @@ import { logger } from "../shared/infrastructure/logging";
 export { run };
 
 async function run(): Promise<void> {
+  logger.info("[DATA-INITIALIZER] STARTED");
   try {
     await findScraperConfig();
-    logger.info("Scraper config found. Skipping data initialization...");
+    logger.info(
+      "[DATA-INITIALIZER] Scraper config found. Skipping data initialization..."
+    );
   } catch (error) {
     if (
       error instanceof UnexpectedNumberOfScraperConfigsFoundError &&
       error.scraperConfigsFound === 0
     ) {
-      logger.info("No scraper config found. Initializing data...");
+      logger.info(
+        "[DATA-INITIALIZER] No scraper config found. Initializing data..."
+      );
       await initializeData();
+      logger.info("[DATA-INITIALIZER] Data initialized successfully");
     }
-
-    return;
+  } finally {
+    logger.info("[DATA-INITIALIZER] FINISHED");
   }
 }
