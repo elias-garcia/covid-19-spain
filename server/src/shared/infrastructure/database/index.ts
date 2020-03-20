977266567cc24e9cadbc47f2756d17cbe44af529
Elias <eliasgarciadev@gmail.com>
2020-03-20 02:36:08 +0100

    Add database logs

--- server/src/shared/infrastructure/database/index.ts
+++ server/src/shared/infrastructure/database/index.ts
@@ -1,12 +1,15 @@
 import * as mongooose from "mongoose";
 
 import { config } from "../config/config";
+import { logger } from "../logging";
 
 export { connect };
 
 async function connect(): Promise<void> {
+  logger.info(`[DB] Connecting to the database on: ${config.MONGO_URI}`);
   await mongooose.connect(config.MONGO_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   });
+  logger.info(`[DB] Database connection established`);
 }
